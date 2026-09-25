6b0fd79cc2b645a75eabf44eeb3ba55f7e1e986d
Thibaut Izard <thibaut.izard@gmail.com>
2026-09-25 21:28:34 +0200

    config(dotnet-api): add formatter and remove default README

--- apps/dotnet-api/Program.cs
+++ apps/dotnet-api/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var animals = new List<Animal>
 {
   new() { Id = 1, Name = "Simba" },
-  new() { Id = 2, Name = "Nala" }
+  new() { Id = 2, Name = "Nala" },
 };
 
 // Add services to the container.
@@ -18,31 +18,40 @@ var animalRoute = app.MapGroup("animals");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-animalRoute.MapGet(string.Empty, () =>
-{
+animalRoute.MapGet(
+  string.Empty,
+  () =>
+  {
     return Results.Ok(animals);
-});
+  }
+);
 
-animalRoute.MapGet("{id:int}", ([FromRoute] int id) =>
-{
+animalRoute.MapGet(
+  "{id:int}",
+  ([FromRoute] int id) =>
+  {
     var animal = animals.SingleOrDefault(e => e.Id == id);
     if (animal == null)
     {
-        return Results.NotFound();
+      return Results.NotFound();
     }
     return Results.Ok(animal);
-});
+  }
+);
 
-animalRoute.MapPost(string.Empty, ([FromBody] Animal animal, HttpContext context) =>
-{
+animalRoute.MapPost(
+  string.Empty,
+  ([FromBody] Animal animal, HttpContext context) =>
+  {
     animal.Id = animals.Max(e => e.Id) + 1;
     animals.Add(animal);
     return Results.Created($"/animals/${animal.Id}", animal);
-});
+  }
+);
 
 app.Run();
